bf0730b843524919878ce5b517f27644c0022fbd
fremag <fremag.gh@gmail.com>
2020-05-14 13:56:21 +0200

    Sessions: display details

--- Pages/Sessions/SessionsComponent.cs
+++ Pages/Sessions/SessionsComponent.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using nats_ui.Data;
@@ -15,16 +16,34 @@ namespace nats_ui.Pages.Sessions
             public override string ToString() => $"{Name}";
         }
 
+        protected class UrlData : ICheckable
+        {
+            public string Url { get; set; }
+            public bool Checked { get; set; }
+        }
+        
+        protected class SubjectData : ICheckable
+        {
+            public string Subject { get; set; }
+            public bool Checked { get; set; }
+        }
+
         [Inject]
         private NatsService NatsService { get; set; }
 
         protected SessionModel Model { get; } = new SessionModel();
+        protected string SelectedName { get; set; }
+        
         protected StandardGridModel<Session> Sessions { get; } = new StandardGridModel<Session>();
+        protected StandardGridModel<UrlData> Urls { get; } = new StandardGridModel<UrlData>();
+        protected StandardGridModel<SubjectData> Subjects { get; } = new StandardGridModel<SubjectData>();
         protected SessionCellFactory GridCellFactory { get; } = new SessionCellFactory();
-
+ 
         protected override Task OnInitializedAsync()
         {
             Sessions.SetData(NatsService.Configuration.Sessions);
+            Urls.SetData(new UrlData[0]);
+            Subjects.SetData(new SubjectData[0]);
             Sessions.SelectedItemChanged += OnSelectedItemChanged;
             Sessions.ItemDoubleClicked += OnItemDoubleClicked;
             return Task.CompletedTask;
@@ -58,6 +77,9 @@ namespace nats_ui.Pages.Sessions
         private void OnSelectedItemChanged(Session session)
         {
             Model.Name = session.Name;
+            SelectedName = session.Name;
+            Subjects.SetData(session.Subscriptions.Select(sub => sub.Subject).Select(subj => new SubjectData {Subject = subj}));
+            Urls.SetData(session.Connections.Select(conn => conn.Url).Select(url => new UrlData {Url = url}));
             InvokeAsync(StateHasChanged);
         }
 
